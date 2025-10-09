8ff3f021ca10c6b753c5103ae7acfec63ecbbd05
FocusedLoop <joshuawlod2003@gmail.com>
2025-10-09 15:49:17 +1000

    terraform please work on my laptop

--- api/infra/aws_run/aws_run_script.sh
+++ api/infra/aws_run/aws_run_script.sh
@@ -2,21 +2,21 @@
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 901444280953.dkr.ecr.ap-southeast-2.amazonaws.com
 
 # API
-docker build -t steam-predictor-api ./api
+docker build -f api/Dockerfile -t steam-predictor-api .
 docker tag steam-predictor-api:latest 901444280953.dkr.ecr.ap-southeast-2.amazonaws.com/steam-predictor-api:latest
 docker push 901444280953.dkr.ecr.ap-southeast-2.amazonaws.com/steam-predictor-api:latest
 
 # Redis
-docker build -t steam-predictor-redis ./api/redis
-docker tag steam-predictor-redis:latest 901444280953.dkr.ecr.ap-southeast-2.amazonaws.com/steam-predictor-redis:latest
+docker pull redis:7-alpine
+docker tag redis:7-alpine 901444280953.dkr.ecr.ap-southeast-2.amazonaws.com/steam-predictor-redis:latest
 docker push 901444280953.dkr.ecr.ap-southeast-2.amazonaws.com/steam-predictor-redis:latest
 
 # Web
-docker build -t steam-predictor-web ./api/web
+docker build -f api/web/Dockerfile -t steam-predictor-web .
 docker tag steam-predictor-web:latest 901444280953.dkr.ecr.ap-southeast-2.amazonaws.com/steam-predictor-web:latest
 docker push 901444280953.dkr.ecr.ap-southeast-2.amazonaws.com/steam-predictor-web:latest
 
 # Sklearn worker
-docker build -t steam-predictor-sklearn ./api/sklearn_worker
+docker build -f api/sklearn_worker/Dockerfile -t steam-predictor-sklearn .
 docker tag steam-predictor-sklearn:latest 901444280953.dkr.ecr.ap-southeast-2.amazonaws.com/steam-predictor-sklearn:latest
 docker push 901444280953.dkr.ecr.ap-southeast-2.amazonaws.com/steam-predictor-sklearn:latest
